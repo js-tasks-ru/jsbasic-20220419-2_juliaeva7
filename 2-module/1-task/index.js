dca665f3d38f55fbc580095fe328935929c3a249
juliaeva7 <juliaeva7@gmail.com>
2022-04-29 12:41:40 +0300

    solved module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    salary = salaries[key];
+    if (!(salary === NaN || salary === Infinity || salary === -Infinity) && isFinite(salary)){
+      sum = sum + +salary;
+    }
+  };
+  return sum;
 }
