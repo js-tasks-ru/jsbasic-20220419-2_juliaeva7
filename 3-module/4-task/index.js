25e0eaa9aa82d4c6662b8131df65593300bc1701
juliaeva7 <juliaeva7@gmail.com>
2022-05-17 14:16:14 +0300

    added 3 module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let usersUnderAge = users.filter((user) => {
+    return user.age <= age;
+  });
+  usersUnderAgeMessage = '';
+  usersUnderAge.forEach((item) => {
+    usersUnderAgeMessage += ( usersUnderAgeMessage !== '' ? '\n' : '' ) + (`${item.name}, ${item.balance}`);
+  });
+  return usersUnderAgeMessage;
 }
