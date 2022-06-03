663c1f3f25ce61644d7f6efdf27ed3dfb71e1701
juliaeva7 <juliaeva7@gmail.com>
2022-06-03 16:08:19 +0300

    added 7 -module

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,97 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
-  constructor(categories) {
+  constructor(categories, elem='') {
     this.categories = categories;
+    this.elem = this.createElement();
   }
+
+  createElement() {
+    let ribbon = document.createElement('div');
+    ribbon.classList.add('ribbon');
+    ribbon.append(createElement( 
+      `<button class="ribbon__arrow ribbon__arrow_left">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>`));
+    
+    this.ribbonInner = document.createElement('nav');
+    this.ribbonInner.classList.add('ribbon__inner');
+    
+    this.categories.map(item => {
+      this.ribbonInner.append(createElement( 
+      `<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`))
+    });
+
+    ribbon.append(this.ribbonInner);
+
+    ribbon.append(createElement( 
+      `<button class="ribbon__arrow ribbon__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>`));
+    
+    this.ribbonArrowLeft   = ribbon.querySelector('.ribbon__arrow_left');
+    this.ribbonArrowRight  = ribbon.querySelector('.ribbon__arrow_right');
+
+    this.ribbonArrowRight.classList.add('ribbon__arrow_visible');
+    
+    this.ribbonInner.addEventListener('scroll', (event) => {this.scroll(event); });
+    
+    this.ribbonArrowRight.addEventListener('click', (event) => {this.ribbonArrowRightFunction(event); });
+    this.ribbonArrowLeft.addEventListener('click', (event) => {this.ribbonArrowLeftFunction(event); });
+    
+    ribbon.addEventListener('click', (event) => {
+      event.preventDefault(); 
+      this.selectRibbonItem(event); 
+    });
+    
+    return ribbon;
+
+  }
+
+  selectRibbonItem(event) {
+
+    const myEventSelectRibbonItem = new CustomEvent('ribbon-select', {
+      bubbles: true, 
+      detail: event.target.dataset.id,
+    });
+   
+    event.target.dispatchEvent(myEventSelectRibbonItem);
+    
+    if (!event.target.classList.contains('ribbon__item')) return;
+    
+    let activated = this.elem.querySelectorAll('.ribbon__item_active');
+    for(let elem of activated) {
+      elem.classList.remove('ribbon__item_active');
+    }
+    event.target.classList.add('ribbon__item_active');
+
+  }
+  
+  scroll(event) {
+    
+    let scrollWidth = this.ribbonInner.scrollWidth;
+    let scrollLeft = this.ribbonInner.scrollLeft;
+    let clientWidth = this.ribbonInner.clientWidth;
+    let scrollRight = scrollWidth - scrollLeft - clientWidth; // число пикселей, например, 100 или 0
+    
+    this.ribbonArrowLeft.classList.add('ribbon__arrow_visible');
+    this.ribbonArrowRight.classList.add('ribbon__arrow_visible');
+    
+    this.ribbonArrowRight.style.display = (scrollRight < 1) ? 'none' : '';
+    this.ribbonArrowLeft.style.display = (scrollLeft < 1) ? 'none' : '';
+
+  }
+
+  ribbonArrowRightFunction(event) {
+    
+    document.querySelector('.ribbon__inner').scrollBy(350, 0);
+  
+  }
+
+  ribbonArrowLeftFunction(event) {
+    
+    document.querySelector('.ribbon__inner').scrollBy(-350, 0);
+      
+  }
+
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,88 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
-  constructor() {
+  constructor(title = '', body = '') {
+    this.title = title;
+    this.body = body;
   }
+
+  setTitle(title) {
+    this.title = title;
+    const modal__title = document.querySelector('.modal__title');
+    if (modal__title) {
+      modal__title.textContent = title;
+    } 
+  }
+  
+  setBody(body) {
+    this.body = body;
+    const modal__body = document.querySelector('.modal__body');
+    if (modal__body) {
+      while (modal__body.firstChild) {
+        modal__body.removeChild(modal__body.firstChild);
+      }
+      modal__body.append(body);
+    } 
+  }
+
+  open() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const modal__overlay = createElement(`<div class="modal__overlay"></div>`);
+    modal.append(modal__overlay);
+
+    const modal__inner = createElement(`<div class="modal__inner"></div>`);
+    
+    const modal__header = createElement(`<div class="modal__header"></div>`);
+    
+    const modal__close = createElement(`
+      <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>`);
+    modal__header.append(modal__close);
+ 
+    const modal__title = createElement(`
+        <h3 class="modal__title">
+          ${this.title}
+        </h3>
+      </div>`);
+    modal__header.append(modal__title);
+
+    const modal__body = document.createElement('div');
+    modal__body.classList.add('modal__body');
+    modal__body.append(this.body);
+
+    modal__inner.append(modal__header);
+    modal__inner.append(modal__body);
+
+    modal.append(modal__inner);
+
+    document.body.append(modal);
+    document.body.classList.add('is-modal-open');
+
+    
+    modal__close.addEventListener('click', (event) => {
+      event.preventDefault(); 
+      this.close(); 
+    });
+
+    document.addEventListener('keydown', function(event) {
+      if (event.code === 'Escape') {
+        modal.remove();
+        document.body.classList.remove('is-modal-open');
+      }
+    });
+    
+  }
+  
+  close() {
+    const modal = document.querySelector('.modal');
+    if (modal) {
+      modal.remove();
+    };
+    document.body.classList.remove('is-modal-open');
+    
+  }
+
 }
